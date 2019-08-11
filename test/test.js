03b0d952d7558cf69dcb9ec15afe80755da1dcb7
iClusterDev <cluster.coredev@gmail.com>
2019-08-11 19:17:12 +0100

    test.js updated

--- test/test.js
+++ test/test.js
@@ -1,10 +1,69 @@
-const assert = require('assert');
-const myPackage = require('../boundle/index.cjs');
+var assert = require('assert');
+var isPlainObject = require('../index.cjs.js');
 
-describe('myPackage', function() {
+describe('isPlainObject', function() {
   describe('on null', function() {
     it('should return false', function() {
-      assert.equal(null, null);
+      assert.equal(isPlainObject(null), false);
+    });
+  });
+
+  describe('on undefined', function() {
+    it('should return false', function() {
+      assert.equal(isPlainObject(), false);
+    });
+  });
+
+  describe('on Number', function() {
+    it('should return false', function() {
+      assert.equal(isPlainObject(100), false);
+    });
+  });
+
+  describe('on String', function() {
+    it('should return false', function() {
+      assert.equal(isPlainObject('foo'), false);
+    });
+  });
+
+  describe('on Boolean', function() {
+    it('should return false', function() {
+      assert.equal(isPlainObject(true), false);
+    });
+  });
+
+  describe('on Array', function() {
+    it('should return false', function() {
+      assert.equal(isPlainObject([1, 2, 3]), false);
+    });
+  });
+
+  describe('on Plain Object', function() {
+    it('should return true', function() {
+      assert.equal(isPlainObject({ a: 100, b: 200 }), true);
+    });
+  });
+
+  describe('on Object.create({})', function() {
+    it('should return true', function() {
+      assert.equal(isPlainObject(Object.create({})), true);
+    });
+  });
+
+  describe('on Object.create(null)', function() {
+    it('should return false', function() {
+      assert.equal(isPlainObject(Object.create(null)), false);
+    });
+  });
+
+  describe('on function', function() {
+    it('should return false', function() {
+      assert.equal(
+        isPlainObject(function() {
+          return true;
+        }),
+        false
+      );
     });
   });
 });
