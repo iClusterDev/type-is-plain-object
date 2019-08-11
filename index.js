33f89c8260cf912ddc1e13111dadb1686973d6e5
iClusterDev <cluster.coredev@gmail.com>
2019-08-11 19:15:00 +0100

    added core func to index

--- index.js
+++ index.js
@@ -1,5 +1,21 @@
-const myPackage = () => {
-  console.log('muPackage');
-};
+import isObject from 'type-is-object';
 
-export default myPackage;
+function isObjectObject(value) {
+  return Object.prototype.toString.call(value) === '[object Object]';
+}
+
+export default function IsPlainObject(value) {
+  if (!isObject(value)) return false;
+
+  if (!isObjectObject(value)) return false;
+
+  var ctor = value.constructor;
+  if (typeof ctor !== 'function') return false;
+
+  var prot = ctor.prototype;
+  if (!isObjectObject(prot)) return false;
+
+  if (!prot.hasOwnProperty('isPrototypeOf')) return false;
+
+  return true;
+}
